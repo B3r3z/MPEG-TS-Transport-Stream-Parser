204b281a7e73515a09bf225c28ba27327e5dc8a7
Bartosz Berezowski <bberezowski7@gmail.com>
2025-04-16 11:18:52 +0200

    First commit

---
+++ build/CMakeFiles/TS-PARSER.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bartosz/Documents/PROJECT/PDM/lab02_MPEG-TS-S1-TRANSPORT_STREAM_PARSER_HEADER/TS_parser.cpp" "CMakeFiles/TS-PARSER.dir/TS_parser.cpp.o" "gcc" "CMakeFiles/TS-PARSER.dir/TS_parser.cpp.o.d"
+  "/home/bartosz/Documents/PROJECT/PDM/lab02_MPEG-TS-S1-TRANSPORT_STREAM_PARSER_HEADER/tsTransportStream.cpp" "CMakeFiles/TS-PARSER.dir/tsTransportStream.cpp.o" "gcc" "CMakeFiles/TS-PARSER.dir/tsTransportStream.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
